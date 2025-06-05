2622348807c20de10220c3336d69cd3f1a956eec
Ek-star <totoekalma@gmail.com>
2025-06-05 17:50:04 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by EKALMA TOTO ALLOY SEMBIRING on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
